394450d7d47af69ef61d3f8f2fd5ea90239cd985
tik-adm <user153015@gmail.com>
2021-07-04 09:52:08 +0200

    commit from tik

--- info.ps1
+++ info.ps1
@@ -1,37 +1,69 @@
-$Global:test = 1
-$test2 = 1
 
+# $Global:fb_folder = "$env:USERPROFILE/fritzbox"
+$fb_folder = "$env:USERPROFILE/fritzbox"
+$newen = '<NewEnable>'
+$close_newen = '</NewEnable>'
+
+function changeenable() {
+    $xml = "$fb_folder/setenable.xml"
+    $xmlcontent = Get-Content $xml
+    # Write-Output $xmlcontent
+    $newen = 'Newenable>'
+    $matches = $xmlcontent | Select-String('(' + $newen + ')([01])') -AllMatches | Foreach-Object { $_.Matches } 
+
+    $groups = $matches | Foreach-Object { $_.Groups }
+    $value = $groups[2].value
+    $value = 1 - $value
+    Write-Host $value
+    $xmlcontent -replace '(' + $newen + ')([01]', '$1$value' | Out-File $xml
+    
+}
+
+changeenable
+
+function regex($action) {
+    $xml = "$fb_folder/$action.xml"
+    # Get-Item $xml
+
+    $xmlcontent = Get-Content $xml
+    Write-Output $xmlcontent
+    
+    $xmlcontent -replace '(<NewEnable>)1(</NewEnable>)', '${1}0$2' | Out-File $xml
+    # $xmlcontent -replace '"(<NewEnable>)1(</NewEnable>)"', '$1.$2' | Out-File $xml
+}
 function fb ($url, $action) {
 
     $control = 'http://192.168.178.1:49000/upnp/control'
     $service = 'urn:dslforum-org:service'
-    $fb_folder = "$env:USERPROFILE/fritzbox"
       
     $location = "$control/$url"
     if ($url -eq 'deviceinfo') {
-        $urlnew = $url
+        $url_ = $url
     }
     else {
-        $urlnew = 'wlanconfiguration'
+        $url_ = 'wlanconfiguration'
     }
 
-    $uri = $service + ':' + $urlnew + ':1#'
+    $uri = $service + ':' + $url_ + ':1#'
 
     $xml = "$fb_folder/$action.xml"
 
     $result = Invoke-WebRequest $location -Headers @{'soapaction' = $uri + $action } -Method post -InFile $xml -ContentType text/xml
 
     Write-Output $result | Select-Object content | Format-List
-    $global:test=2
-    $test2=2
+    # $global:test=2
+    # $test2=2
 
 }
+$action = 'getinfo'
+$url = 'deviceinfo'
+# fb $url $action
 $action = 'setenable'
+# regex $action
 $url = 'wlanconfig1'
 # fb $url $action
 $action = 'getinfo'
-fb $url $action
-# $url = 'deviceinfo'
+# fb $url $action
 # Write-Host $Global:test
 # Write-Host $test2
 
--- info.py
+++ info.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 from os import path
 from os.path import join
-from typing import Pattern
 import requests
-import xml.etree.ElementTree as ET
 import xmltodict
-import json
 import re
 
 headers = {'content-type': 'text/xml'}
@@ -19,42 +16,15 @@ fb_folder = path.dirname(__file__)
 
 def main():
     change_enable()
-    # fb('wlanconfig1', 'setenable')
-    # xml_ = fb('wlanconfig1', 'getinfo')
-    # print(xml)
-    # fb('deviceinfo', 'getinfo')
+    # xml_ = fb('wlanconfig1', 'setenable')
+    # xml_ = fb('deviceinfo', 'getinfo')
+    xml_ = fb('wlanconfig1', 'getinfo')
+    print(xml_)
     # test()
 
 
-def change_enable():
-
-    xml_ = join(fb_folder, 'setenable.xml')
-
-    with open(xml_, 'r') as file_:
-        newen = '<NewEnable>'
-        close_newen = '</NewEnable>'
-
-        pattern = newen+'([01])'+close_newen
-        str_ = ''
-        for line in file_:
-            print(line)
-            if match := re.match(pattern, line):
-                val = match.group(1)
-                val = 1-int(val)
-                pattern = '(' + newen+')[01]('+close_newen+')'
-                str_ += re.sub(pattern,
-                                '\g<1>'+str(val)+'\g<2>', line)
-                continue
-            str_ += line
-
-        # print(str_)
-    with open(xml_, 'w') as file_:
-        file_.write(str_)
-
-
 def fb(url, action):
-
-    # url = "wlanconfig1"
+    print('.. fb started ..')
     location = control + '/' + url
     if url == 'deviceinfo':
         url_ = url
@@ -63,47 +33,55 @@ def fb(url, action):
     uri = service + ':' + url_ + ':1#'
 
     xml_ = join(fb_folder, action + '.xml')
+
     headers['soapaction'] = uri + action
 
     with open(xml_, 'r') as f:
         xml = f.read()
-    # print(location,xml_,headers)
+    print('..', xml_, 'parsed..')
     response = requests.post(location, data=xml, headers=headers)
-    # if (url == 'wlanconfig1' and action == 'getinfo'):
-    #     parseinfo(response)
-    #     return
-    return response.content
+    if (url == 'wlanconfig1' and action == 'getinfo'):
+        return parseinfo(response, 'newenable')
+    elif (url == 'deviceinfo' and action == 'getinfo'):
+        return parseinfo(response, 'newsoftwareversion')
 
 
-def test():
-    print(json.dumps(xmltodict.parse("""
-  <mydocument has="an attribute">
-    <and>
-      <many>elements</many>
-      <many>more elements</many>
-    </and>
-    <plus a="complex">
-      element as well
-    </plus>
-  </mydocument>
-"""), indent=4))
+def parseinfo(response, key):
+    print('..parse_info started..', key)
 
-
-def parseinfo(response):
-    # for line in parse:
-    #     print(line)
-    # print(parse.content)
-    tree = ET.fromstring(response.content)
-    # for elt in e.iter():
-    #     print(elt.tag, elt.helper_xml)
-
-    # print(tree,type(tree))
-    # print(tree)
     # print(xmltodict.parse(response.content))
-    # print(json.dumps(xmltodict.parse(response.content)))
-    for elem in tree.iter():
-        print(elem)
+    xmld = xmltodict.parse(response.content.lower(), dict_constructor=dict)
+    env = xmld['s:envelope']
+    body = env['s:body']
+    inforesp = body['u:getinforesponse']
+    # result = inforesp['newenable']
+    # result = inforesp
+    result = inforesp[key]
+    return result
 
+
+def change_enable():
+    print('..change_en started..')
+    xml_ = join(fb_folder, 'setenable.xml')
+
+    with open(xml_, 'r') as file_:
+        newen = '<NewEnable>'
+        close_newen = '</NewEnable>'
+
+        str_ = ''
+        for line in file_:
+            if match := re.match(newen+'([01])', line):
+                value = match.group(1)
+                value = 1-int(value)
+                str_ += re.sub(f'({newen})[01]',
+                               f'\g<1>{str(value)}', line)
+                continue
+            str_ += line
+
+        # print(str_)
+    with open(xml_, 'w') as file_:
+        file_.write(str_)
+    # return fb('wlanconfig1', 'getinfo')
 
 if __name__ == '__main__':
     main()
